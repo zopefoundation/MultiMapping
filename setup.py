6cf0f7f831c6e0141195635a8b61bc31383f9790
Michael Howitz <m.howitz@minddistrict.com>
2026-01-22 09:54:21 +0100

    Update Python version support. (#10)
    * Drop support for Python 3.8, 3.9.
    
    * Add support for Python 3.13, 3.14.
    
    * Configuring for pure-python
    
    * Update Python version support.
    
    * Move package metadata from setup.py to pyproject.toml.
    
    * Change the code.
    
    * Update pyproject.toml
    
    Co-authored-by: Jens Vagelpohl <jens@plyp.com>
    
    ---------
    
    Co-authored-by: Jens Vagelpohl <jens@plyp.com>

--- setup.py
+++ setup.py
@@ -12,42 +12,10 @@
 #
 ##############################################################################
 
-from setuptools import find_packages
 from setuptools import setup
 
 
-version = '6.0.dev0'
+version = "6.0.dev0"
 
-setup(
-    name='MultiMapping',
-    version=version,
-    url='https://github.com/zopefoundation/MultiMapping',
-    license='ZPL 2.1',
-    description="Special MultiMapping objects used in Zope.",
-    classifiers=[
-        "Development Status :: 6 - Mature",
-        "Environment :: Web Environment",
-        "Framework :: Zope :: 5",
-        "License :: OSI Approved :: Zope Public License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    long_description=(open('README.txt').read() + '\n' +
-                      open('CHANGES.txt').read()),
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    python_requires='>=3.8',
-    install_requires=['ExtensionClass'],
-    include_package_data=True,
-    zip_safe=False,
-)
+# See pyproject.toml for package metadata
+setup()
